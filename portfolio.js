ad9fe421eeec82c715a62df9eebee8505b4ac229
KeerthanaN06 <133489674+KeerthanaN06@users.noreply.github.com>
2023-10-31 19:11:32 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// JavaScript for interactive features
+
+// Example of a simple alert when the user clicks on the "Contact Me" section
+const contactSection = document.getElementById('contact');
+contactSection.addEventListener('click', () => {
+    alert('Thank you for your interest! Please feel free to contact me at yourname@example.com');
+});
